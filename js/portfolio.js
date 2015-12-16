5325945aab69f91bc8c2ba4a06f14bc47fecaeec
EugenRakhimov <gin3002@gmail.com>
2015-12-16 21:33:22 +1300

    added about block

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,6 +5,40 @@
       $('#preloader').delay(300).fadeOut('slow');
     });
   $(document).ready(function() {
+    /* ---------------------------------------------- /*
+     * Smooth scroll / Scroll To Top
+    /* ---------------------------------------------- */
+
+    $('a[href*=#]').bind("click", function(e){
+           
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+      }, 1000);
+      e.preventDefault();
+    });
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.scroll-up').fadeIn();
+      } else {
+        $('.scroll-up').fadeOut();
+      }
+    });
+
+    /* ---------------------------------------------- /*
+     * Navbar
+    /* ---------------------------------------------- */
+
+    $('.header').sticky({
+      topSpacing: 0
+    });
+
+    $('body').scrollspy({
+      target: '.header',
+      offset: 70
+    });
+
   
     $("#home").height($(window).height());
 
